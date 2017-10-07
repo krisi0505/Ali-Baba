f201f351fbb86428a3d44899921d179d0409ba28
krisi0505 <krisi0505@abv.bg>
2017-10-07 22:15:26 +0300

    add content

--- AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
@@ -2,6 +2,8 @@ package com.project.krisi.alibaba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        int[] numbers={1,2,3,4};
+
+        LinearLayout layout = (LinearLayout)this.findViewById(R.id.precious);
+
+        TextView coin = (TextView)this.findViewById(R.id.precious_item);
+        coin.setText("hallo hallo");
+
+        
     }
 }
