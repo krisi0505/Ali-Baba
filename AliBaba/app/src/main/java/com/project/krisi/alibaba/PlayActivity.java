ebf86a973cedb5b038b535fed6f771cd82c56842
krisi0505 <krisi0505@abv.bg>
2017-10-07 23:52:49 +0300

    distribute textviews evenly

--- AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
@@ -2,6 +2,8 @@ package com.project.krisi.alibaba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,13 +14,27 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        int[] numbers={1,2,3,4};
+        LinearLayout ll=(LinearLayout)findViewById(R.id.precious);
 
-        LinearLayout layout = (LinearLayout)this.findViewById(R.id.precious);
+        final int N = 5; // total number of textviews to add
 
-        TextView coin = (TextView)this.findViewById(R.id.precious_item);
-        coin.setText("hallo hallo");
+        final TextView[] myTextViews = new TextView[N]; // create an empty array;
 
-        
+        for (int i = 1; i <= N; i++) {
+            // create a new textview
+            final TextView rowTextView = new TextView(this);
+
+            // set some properties of rowTextView or something
+            rowTextView.setText(""+i);
+            rowTextView.setLayoutParams(new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
+            rowTextView.setTextSize(28);
+            rowTextView.setGravity(17);//center
+
+            // add the textview to the linearlayout
+            ll.addView(rowTextView);
+
+            // save a reference to the textview for later
+            myTextViews[i-1] = rowTextView;
+        }
     }
 }
