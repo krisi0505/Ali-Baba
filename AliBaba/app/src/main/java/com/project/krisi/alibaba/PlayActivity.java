6c7ed568048e7a68646dbea001de98880a6fd3c6
krisi0505 <krisi0505@abv.bg>
2017-10-08 13:45:55 +0300

    add pictures and prices

--- AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
@@ -3,9 +3,11 @@ package com.project.krisi.alibaba;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.util.Random;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -14,27 +16,46 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        LinearLayout ll=(LinearLayout)findViewById(R.id.precious);
+        LinearLayout jemContainer =(LinearLayout)findViewById(R.id.precious);
 
-        final int N = 5; // total number of textviews to add
+        final int N = 3; // total number of textviews to add
 
-        final TextView[] myTextViews = new TextView[N]; // create an empty array;
+        final TextView[] prices = new TextView[N]; // create an empty array;
+        final LinearLayout[] items = new LinearLayout[N];
 
         for (int i = 1; i <= N; i++) {
-            // create a new textview
-            final TextView rowTextView = new TextView(this);
+            //create a random price
+            Random rand = new Random();
+            int  randomPrice = rand.nextInt(500) + 1;
 
-            // set some properties of rowTextView or something
-            rowTextView.setText(""+i);
-            rowTextView.setLayoutParams(new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
-            rowTextView.setTextSize(28);
-            rowTextView.setGravity(17);//center
+            //create an imageview
+            final ImageView img = new ImageView(this);
+            img.setImageResource(R.drawable.diamond);
+            img.setLayoutParams(new LayoutParams(64, 64));
+
+            // create a new textview
+            final TextView price = new TextView(this);
+
+            // set some properties of textview
+            price.setText("$" + randomPrice);
+            price.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
+            price.setTextSize(28);
+            price.setGravity(17);//center
+
+            final LinearLayout item = new LinearLayout(this);
+            item.setLayoutParams(new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
+            item.setGravity(17);//center
+            item.setOrientation(LinearLayout.VERTICAL);
 
             // add the textview to the linearlayout
-            ll.addView(rowTextView);
+            item.addView(img);
+            item.addView(price);
+
+            //add the item to the linearlayout
+            jemContainer.addView(item);
 
             // save a reference to the textview for later
-            myTextViews[i-1] = rowTextView;
+            prices[i-1] = price;
         }
     }
 }
