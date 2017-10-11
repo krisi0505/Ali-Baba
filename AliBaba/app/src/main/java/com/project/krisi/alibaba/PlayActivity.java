26c36789340e6ff3dc142498c915a51fcac22e78
krisi0505 <krisi0505@abv.bg>
2017-10-11 20:21:00 +0300

    visualise db table

--- AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/PlayActivity.java
@@ -4,18 +4,16 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.Random;
 
--- AliBaba/app/src/main/java/com/project/krisi/alibaba/ScoresActivity.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/ScoresActivity.java
@@ -2,6 +2,10 @@ package com.project.krisi.alibaba;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,6 +18,34 @@ public class ScoresActivity extends AppCompatActivity {
 
         List<HighScores> scores = HighScores.listAll(HighScores.class);
 
+        //List<HighScores> scores = HighScores.findWithQuery(HighScores.class, "SELECT * FROM HighScores LIMIT 5", "myscores");
+
+        TableLayout table = (TableLayout)findViewById(R.id.table_layout);
         
+        for(HighScores result:scores){
+            TableRow row = new TableRow(this);
+            row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            row.setGravity(16);//center_horizontal
+
+            String name = result.getName();
+            final TextView tvName = new TextView(this);
+            tvName.setText(name);
+            tvName.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            tvName.setTextSize(18);
+            tvName.setGravity(17);//center
+
+            int points = result.getScore();
+            final TextView tvPoints = new TextView(this);
+            tvPoints.setText("" + points);
+            tvPoints.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            tvPoints.setTextSize(18);
+            tvPoints.setGravity(17);//center
+
+            TableRow.LayoutParams params = new TableRow.LayoutParams();
+            params.span = 6;
+            row.addView(tvName, params);
+            row.addView(tvPoints, params);
+            table.addView(row);
+        }
     }
 }
