5d28bfa1b5c15762b453c1cc5a5e98286a04a447
krisi0505 <krisi0505@abv.bg>
2017-10-19 00:00:03 +0300

    add custom view item

--- AliBaba/app/src/main/java/com/project/krisi/alibaba/activities/PlayActivity.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/activities/PlayActivity.java
@@ -1,23 +1,16 @@
 package com.project.krisi.alibaba.activities;
 
-import android.content.ClipData;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.DragEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 import com.project.krisi.alibaba.R;
-
-import java.util.Random;
+import com.project.krisi.alibaba.views.ItemView;
 
 public class PlayActivity extends KnapsackActivity {
 
@@ -29,88 +22,91 @@ public class PlayActivity extends KnapsackActivity {
         LinearLayout jemContainer =(LinearLayout)findViewById(R.id.precious);
 
         final int score = 0;
-        final int N = 3; // total number of textviews to add
+        final int N = 5; // total number of textviews to add
 
         final int[] prices = new int[N]; // create an empty array;
         final int[] volumes = new int[N]; // create an empty array;
 
         for (int i = 1; i <= N; i++) {
+
+            ItemView item = new ItemView(this);
+
             //create a random price
-            Random rand = new Random();
-            int  randomPrice = rand.nextInt(500) + 1;
-
-            //create an imageview
-            final ImageView img = new ImageView(this);
-            img.setLayoutParams(new LayoutParams(64, 64));
-            if(randomPrice<100){
-                img.setImageResource(R.drawable.coins);
-            }
-            else if(randomPrice<200){
-                img.setImageResource(R.drawable.emerald);
-            }
-            else if(randomPrice<300){
-                img.setImageResource(R.drawable.ruby);
-            }
-            else if(randomPrice<400){
-                img.setImageResource(R.drawable.ring);
-            }
-            else{
-                img.setImageResource(R.drawable.diamond);
-            }
-
-            // create a new textview
-            final TextView price = new TextView(this);
-
-            // set some properties of textview
-            price.setText("$" + randomPrice);
-            price.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
-            price.setTextSize(20);
-            price.setTextColor(Color.WHITE);
-            price.setGravity(17);//center
-
-            Random randV = new Random();
-            int  randomVolume = randV.nextInt(16) + 1;
-
-            // create a new textview
-            final TextView volume = new TextView(this);
-
-            // set some properties of textview
-            volume.setText(randomVolume + "L");
-            volume.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
-            volume.setTextSize(20);
-            volume.setTextColor(Color.WHITE);
-            volume.setGravity(17);//center
-
-            //create a linearlayout for each item
-            final LinearLayout item = new LinearLayout(this);
-            item.setLayoutParams(new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
-            item.setGravity(17);//center
-            item.setOrientation(LinearLayout.VERTICAL);
-            item.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    ClipData data = ClipData.newPlainText("","");
-                    View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        v.startDragAndDrop(data, myShadowBuilder, v,0);
-                    } else {
-                        v.startDrag(data, myShadowBuilder, v,0);
-                    }
-                    return true;
-                }
-            });
-
-            // add the textview to the linearlayout
-            item.addView(img);
-            item.addView(price);
-            item.addView(volume);
+//            Random rand = new Random();
+//            int  randomPrice = rand.nextInt(500) + 1;
+//
+//            //create an imageview
+//            final ImageView img = new ImageView(this);
+//            img.setLayoutParams(new LayoutParams(64, 64));
+//            if(randomPrice<100){
+//                img.setImageResource(R.drawable.coins);
+//            }
+//            else if(randomPrice<200){
+//                img.setImageResource(R.drawable.emerald);
+//            }
+//            else if(randomPrice<300){
+//                img.setImageResource(R.drawable.ruby);
+//            }
+//            else if(randomPrice<400){
+//                img.setImageResource(R.drawable.ring);
+//            }
+//            else{
+//                img.setImageResource(R.drawable.diamond);
+//            }
+//
+//            // create a new textview
+//            final TextView price = new TextView(this);
+//
+//            // set some properties of textview
+//            price.setText("$" + randomPrice);
+//            price.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
+//            price.setTextSize(20);
+//            price.setTextColor(Color.WHITE);
+//            price.setGravity(17);//center
+//
+//            Random randV = new Random();
+//            int  randomVolume = randV.nextInt(16) + 1;
+//
+//            // create a new textview
+//            final TextView volume = new TextView(this);
+//
+//            // set some properties of textview
+//            volume.setText(randomVolume + "L");
+//            volume.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
+//            volume.setTextSize(20);
+//            volume.setTextColor(Color.WHITE);
+//            volume.setGravity(17);//center
+//
+//            //create a linearlayout for each item
+//            final LinearLayout item = new LinearLayout(this);
+//            item.setLayoutParams(new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
+//            item.setGravity(17);//center
+//            item.setOrientation(LinearLayout.VERTICAL);
+//            item.setOnTouchListener(new View.OnTouchListener() {
+//                @Override
+//                public boolean onTouch(View v, MotionEvent event) {
+//                    ClipData data = ClipData.newPlainText("","");
+//                    View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                        v.startDragAndDrop(data, myShadowBuilder, v,0);
+//                    } else {
+//                        v.startDrag(data, myShadowBuilder, v,0);
+//                    }
+//                    return true;
+//                }
+//            });
+//
+//            // add the textview to the linearlayout
+//            item.addView(img);
+//            item.addView(price);
+//            item.addView(volume);
 
             //add the item to the linearlayout
             jemContainer.addView(item);
 
             // save a reference to the textview for later
-            prices[i-1] = randomPrice;
-            volumes[i-1] = randomVolume;
+            prices[i-1] = item.mPrice;
+            volumes[i-1] = item.mVolume;
         }
 
         final TextView capacity = (TextView)findViewById(R.id.capacity_number);
@@ -126,11 +122,11 @@ public class PlayActivity extends KnapsackActivity {
                 int dragEvent = event.getAction();
 
                 if(dragEvent == DragEvent.ACTION_DROP) {
-                    final ViewGroup view = (ViewGroup) event.getLocalState();
+                    final ItemView view = (ItemView) event.getLocalState();
 
-                    final TextView tvPrice = (TextView) view.getChildAt(1);
+                    final TextView tvPrice = (TextView) view.mTVPrice;
                     String price = tvPrice.getText().toString();
-                    final TextView tvVolume = (TextView) view.getChildAt(2);
+                    final TextView tvVolume = (TextView) view.mTVVolume;
                     String volume = tvVolume.getText().toString();
                     int priceNumber = Integer.parseInt(price.substring(1));
                     int volumeNumber = Integer.parseInt(volume.substring(0, volume.length() - 1));
--- AliBaba/app/src/main/java/com/project/krisi/alibaba/views/ItemView.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/views/ItemView.java
@@ -1,14 +1,109 @@
 package com.project.krisi.alibaba.views;
 
+import android.content.ClipData;
 import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.project.krisi.alibaba.R;
+
+import java.util.Random;
 
 /**
  * Created by Krisi on 17.10.2017 г..
  */
 
-public class ItemView extends View {
+public class ItemView extends LinearLayout {
+    private ImageView mImg;
+    public TextView mTVPrice;
+    public TextView mTVVolume;
+    public int mPrice;
+    public int mVolume;
+    private static final int maxPrice = 500;
+    private static final int maxVolume = 16;
+    private static final int maxCoinCost = 100;
+    private static final int maxRubyCost = 200;
+    private static final int maxEmeraldCost = 300;
+    private static final int maxRingCost = 400;
+
     public ItemView(Context context) {
         super(context);
+        initializeViews(context);
+    }
+
+    public ItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeViews(context);
+    }
+
+    public ItemView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initializeViews(context);
+    }
+
+    private void initializeViews(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.item_view, this);
+        this.onFinishInflate();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        Random randP = new Random();
+        int randomPrice = randP.nextInt(maxPrice) + 1;
+
+        mImg = (ImageView)this.findViewById(R.id.gem);
+
+        if(randomPrice < maxCoinCost){
+            mImg.setImageResource(R.drawable.coins);
+        }
+        else if(randomPrice < maxRubyCost){
+            mImg.setImageResource(R.drawable.ruby);
+        }
+        else if(randomPrice < maxEmeraldCost){
+            mImg.setImageResource(R.drawable.emerald);
+        }
+        else if(randomPrice < maxRingCost){
+            mImg.setImageResource(R.drawable.ring);
+        }
+        else{
+            mImg.setImageResource(R.drawable.diamond);
+        }
+
+        mTVPrice = (TextView)this.findViewById(R.id.price);
+        mTVPrice.setText("$" + randomPrice);
+
+        Random randV = new Random();
+        int  randomVolume = randV.nextInt(maxVolume) + 1;
+
+        mTVVolume = (TextView)this.findViewById(R.id.volume);
+        mTVVolume.setText(randomVolume + "L");
+
+        this.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ClipData data = ClipData.newPlainText("","");
+                View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    v.startDragAndDrop(data, myShadowBuilder, v,0);
+                } else {
+                    v.startDrag(data, myShadowBuilder, v,0);
+                }
+                return true;
+            }
+        });
+
+        mPrice = randomPrice;
+        mVolume = randomVolume;
+
     }
 }
