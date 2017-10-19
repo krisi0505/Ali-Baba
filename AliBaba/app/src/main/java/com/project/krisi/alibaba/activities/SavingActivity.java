392352cbfaf96a031057a163372334c3f8347013
krisi0505 <krisi0505@abv.bg>
2017-10-19 17:48:57 +0300

    add toasts and data validation

--- AliBaba/app/src/main/java/com/project/krisi/alibaba/activities/SavingActivity.java
+++ AliBaba/app/src/main/java/com/project/krisi/alibaba/activities/SavingActivity.java
@@ -1,5 +1,6 @@
 package com.project.krisi.alibaba.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.project.krisi.alibaba.models.HighScores;
 import com.project.krisi.alibaba.R;
+import com.project.krisi.alibaba.models.HighScores;
 
 public class SavingActivity extends AppCompatActivity {
 
@@ -35,6 +37,33 @@ public class SavingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText et = (EditText)findViewById(R.id.edit_text);
                 String name = et.getText().toString();
+
+                //Check if name is too long or too short
+                final int minLength = 2;
+                final int maxLength = 15;
+                if(name.length() < minLength){
+                    Context context = getApplicationContext();
+                    CharSequence text = "Your name has to be longer";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    return;
+                }
+                if(maxLength < name.length()){
+                    Context context = getApplicationContext();
+                    CharSequence text = "Your name has to be shorter";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    return;
+                }
+
+                //Check if name contains only letters and numbers
+                if(!name.matches("[a-zA-Z0-9]+")){
+                    return;
+                }
 
                 HighScores player = new HighScores(name, score);
                 player.save();
